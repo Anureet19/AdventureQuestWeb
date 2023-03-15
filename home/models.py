d8ba7d9a95b155aec22d8e949d46c83e53a03cd4
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2023-03-15 14:53:34 -0400

    created models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Tier(models.Model):
+    name = models.CharField(max_length=30, default="Gold")
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Package(models.Model):
+    PACKAGE_STATUS = (
+        ("1", "Available"),
+        ("2", "Not Available"),
+    )
+
+    PACKAGE_TYPE = (
+        ("1", "Season"),
+        ("2", "Monthly"),
+        ("3", "Daily"),
+    )
+
+    package_type = models.CharField(max_length=50, choices=PACKAGE_TYPE)
+    price_factor = models.IntegerField()
+    tier = models.ForeignKey(Tier, on_delete=models.CASCADE)
+    status = models.CharField(choices=PACKAGE_STATUS, max_length=15)
+
+    def __str__(self):
+        return self.package_type
+
+
+class Reservation(models.Model):
+    entry_date = models.DateField(auto_now=False)
+    expiry_date = models.DateField()
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    guest = models.ForeignKey(User, on_delete=models.CASCADE)
+    booking_id = models.CharField(max_length=100, default="null")
+
+    def __str__(self):
+        return self.guest.username
