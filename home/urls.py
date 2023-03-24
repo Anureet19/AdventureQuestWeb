55c97c79c44dffa86256ad7a71d23fbbd2446dd1
Harmanjot Singh <harmanjotsuri@gmail.com>
2023-03-24 00:57:38 -0400

    restructured html files

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pages/login/', views.UserLoginView.as_view(), name='login'),
+    path('pages/logout/', views.user_logout_view, name='logout'),
+    path('pages/register/', views.registration, name='register'),
 ]
