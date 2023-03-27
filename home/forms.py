f6149748833bfaa0f0cb35859c8d642fdf53c757
Dhruv Patel <80629263+DhruvPatel96@users.noreply.github.com>
2023-03-27 03:38:47 -0400

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -38,31 +38,42 @@ class LoginForm(AuthenticationForm):
 
 
 class UserPasswordResetForm(PasswordResetForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        'class': 'form-control'
-    }))
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
 
 
 class UserSetPasswordForm(SetPasswordForm):
-    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }), label="New Password")
-    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }), label="Confirm New Password")
+    new_password1 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="New Password"
+    )
+    new_password2 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="Confirm New Password"
+    )
 
 
 class UserPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }), label='Old Password')
-    new_password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }), label="New Password")
-    new_password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(attrs={
-        'class': 'form-control'
-    }), label="Confirm New Password")
+    old_password = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label='Old Password'
+    )
 
+    new_password1 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="New Password"
+    )
+
+    new_password2 = forms.CharField(
+        max_length=50,
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="Confirm New Password"
+    )
 
 class GroupPassForm(forms.ModelForm):
     class Meta:
